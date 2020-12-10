e950198db48be1f52b437fc8d796b0e2c70b88b3
ayninur <nuralaynabdulqadir@gmail.com>
2020-12-10 11:10:12 -0500

    frontend

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Nav, NavLink } from './NavbarElements'
+
+
+const Navbar = () => {
+    return (
+        <>
+            <Nav>
+                <NavLink to='/'>Pizza</NavLink>
+            </Nav>
+        </>
+    )
+}
+
+export default Navbar
